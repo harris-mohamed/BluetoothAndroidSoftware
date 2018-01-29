d8c26a95871a7a3326b2424699258f540e5c118a
Harris Mohamed <harrism436@gmail.com>
2018-01-28 20:00:00 -0600

    Started Modular Bluetooth Program

--- CAN Network Arduino Code/Modular_Bluetooth_Test/Modular_Bluetooth_Test.ino
+++ CAN Network Arduino Code/Modular_Bluetooth_Test/Modular_Bluetooth_Test.ino
@@ -6,50 +6,160 @@
 #include <mcp2515_defs.h>
 
 
-SoftwareSerial BT = SoftwareSerial(3,4);    //RX, TX
+SoftwareSerial BT = SoftwareSerial(3, 4);   //RX, TX
+
+String company[9];
+String company_out[9];
+String rev_version[9];
+String rev_version_out[9];
+String state[8];
+String state_out[8]; 
+String voltage[7];
+String voltage_out[7];
+String current[7];
+String current_out[7];
+String energy[9];
+String energy_out[9];
+String charge[8];
+String temp[7];
+String resistance[7];
 
 void setup() {
-  Serial.begin(9600); 
+  BT.begin(9600);
+  Serial.begin(9600);
   delay(1000);
-  BT.write("test");
-  Serial.println("CAN bluetooth send test"); 
-  BT.listen(); 
-   
-    if(Canbus.init(CANSPEED_500)) { 
+  Serial.println("CAN bluetooth send test");
+  //BT.listen();
+
+  if (Canbus.init(CANSPEED_500)) {
     Serial.println("CAN Init ok");
-     BT.begin(9600);
-    //BT.write("dope");
-    Serial.write("h");
-    }
+  }
   else
   {
     Serial.println("Can't init CAN");
-    BT.write("hi");
   }
-
   delay(1000);
 }
 
 
 
-void loop(){
+void loop() {
   tCAN message;
-  //Bluetooth();
-if (mcp2515_check_message()) 
+  if (mcp2515_check_message())
   {
-    if (mcp2515_get_message(&message)) 
-  {
-        if(message.id == 0x621)  
-             {
-                for(int i=0;i<message.header.length;i++) 
-                { 
-                  BT.write(message.data[i]);
+    if (mcp2515_get_message(&message))
+    {
+      if (message.id == 0x620)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          company[x] = message.data[i];
+        }
 
-                }
+      }
 
-             }   
-           }}
-    delay(1000);                        
+      else if (message.id == 0x621)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          rev_version[x] = message.data[i];
+        }
+
+      }
+
+      else if (message.id == 0x622)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          state[x] = message.data[i];
+        }
+
+      }
+
+      else if (message.id == 0x623)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          voltage[x] = message.data[i];
+        }
+
+      }
+
+      else if (message.id == 0x624)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          current[x] = message.data[i];
+        }
+
+      }
+
+      else if (message.id == 0x625)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          energy[x] = message.data[i];
+        }
+
+      }
+
+      else if (message.id == 0x626)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          charge[x] = message.data[i];
+        }
+
+      }
+
+      else if (message.id == 0x627)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          temp[x] = message.data[i];
+        }
+
+      }
+
+      else if (message.id == 0x628)
+      {
+        for (int i = 0; i < message.header.length; i++)
+        {
+          int x = i + 1;
+          rev_version[x] = message.data[i];
+        }
+
+      }
+
+    }
+  }
+  calculate_and_format();
+  bluetooth_out();
+  delay(1000);
 }
 
+void calculate_and_format() {
+  company[0] = "#";
+  rev_version[0] = "#";
+  state[0] = "#";
+  voltage[0] = #";
+  current[0] = "#";
+  energy[0] = "#";
+  charge[0] = "#";
+  temp[0] = "#";
+  resistance[0] = "#";
 
+    
+}
+
+void bluetooth_out() {
+
+}
